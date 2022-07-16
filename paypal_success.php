3a8a5a0d3405c31a8453be02f97ddfa23929f641
Muhammad Habib <mhabib555@gmail.com>
2022-07-16 13:16:49 +0300

    kkk

--- paypal_success.php
+++ paypal_success.php
@@ -5,7 +5,10 @@ define("USE_SANDBOX",1);
 define("LOG_FILE", "paypal_ipn.log");
 
 
-error_reporting(1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 
 $rawInputData = file_get_contents('php://input');
 $rawInputDataArr = explode("&", $rawInputData);
